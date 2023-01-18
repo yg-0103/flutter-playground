739c0e7010f5c46a34ab7f07c8a4cd643fe6de27
김연구 <yeongoo.kim@qualson.com>
2023-01-19 00:17:33 +0900

    feat: Add Pomodoros Timer

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/currency_card.dart';
-import './widgets/button.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(App());
@@ -10,136 +9,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color.fromRGBO(18, 18, 18, 0.8),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 60,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Text(
-                          'Hey, Selena',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 26,
-                              fontWeight: FontWeight.w600),
-                        ),
-                        SizedBox(
-                          height: 2,
-                        ),
-                        Text('Welcome back',
-                            style: TextStyle(
-                              color: Color.fromRGBO(255, 255, 255, 0.8),
-                            ))
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  'Total Balance',
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 22,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  '\$5 194 482',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 44,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Button(
-                      bgColor: Color(0xFFF1b33B),
-                      text: 'Transfer',
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      bgColor: Color(0xFF1F2123),
-                      text: 'Request',
-                      textColor: Colors.white,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    const Text('Wallets',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 34,
-                            fontWeight: FontWeight.w700)),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.8),
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const CurrencyCard(
-                  amount: '6 428',
-                  code: 'EUR',
-                  icon: Icons.euro_rounded,
-                  name: 'Euro',
-                  isInverted: false,
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -20),
-                  child: const CurrencyCard(
-                    amount: '6 428',
-                    code: 'BTC',
-                    icon: Icons.currency_bitcoin,
-                    name: 'Bitcoin',
-                    isInverted: true,
-                  ),
-                ),
-                Transform.translate(
-                  offset: const Offset(0, -40),
-                  child: const CurrencyCard(
-                    amount: '6 428',
-                    code: 'Dollar',
-                    icon: Icons.attach_money_rounded,
-                    name: 'USD',
-                    isInverted: false,
-                  ),
-                ),
-              ],
-            ),
+      theme: ThemeData(
+          backgroundColor: const Color(0xFFE7626C),
+          textTheme: const TextTheme(
+            headline1: TextStyle(color: Color(0XFF232B55)),
           ),
-        ),
-      ),
+          cardColor: const Color(0xFFF4EDDB)),
+      home: const Home(),
     );
   }
 }
